2188603b5b7b25aed2782643cf6a12e0aa565409
Sajjad Ahmad <jogimar14@gmail.com>
2021-02-07 16:16:19 +0500

    add grid of menu cards for menu container

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,10 +1,37 @@
 const Menu = (() => {
   const addMenu = () => {
-    const h1 = document.createElement('h1');
-    h1.setAttribute('id','menu-content');
-    h1.innerHTML = 'I am a menu page';
+    const menuContainer = document.createElement('div');
+    menuContainer.setAttribute('id', 'menuContainer');
+    const menu1 = document.createElement('div');
+    menu1.setAttribute('class', 'menu-box');
+    menu1.textContent = 'I am menu';
+    menuContainer.appendChild(menu1);
+    const menu2 = document.createElement('div');
+    menu2.setAttribute('class', 'menu-box');
+    menu2.textContent = 'I am menu';
+    menuContainer.appendChild(menu2);
+    const menu3 = document.createElement('div');
+    menu3.setAttribute('class', 'menu-box');
+    menu3.textContent = 'I am menu';
+    menuContainer.appendChild(menu3);
+    const menu4 = document.createElement('div');
+    menu4.setAttribute('class', 'menu-box');
+    menu4.textContent = 'I am menu';
+    menuContainer.appendChild(menu4);
+    const menu5 = document.createElement('div');
+    menu5.setAttribute('class', 'menu-box');
+    menu5.textContent = 'I am menu';
+    menuContainer.appendChild(menu5);
+    const menu6 = document.createElement('div');
+    menu6.setAttribute('class', 'menu-box');
+    menu6.textContent = 'I am menu';
+    menuContainer.appendChild(menu6);
+    const menu7 = document.createElement('div');
+    menu7.setAttribute('class', 'menu-box');
+    menu7.textContent = 'I am menu';
+    menuContainer.appendChild(menu7);
     const container = document.getElementsByClassName('content-container')[0]
-    container.appendChild(h1);
+    container.appendChild(menuContainer);
   };
   return { addMenu };
 })();
